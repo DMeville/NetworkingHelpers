a39644f35f4e8cd0a13f3c358a4f4368ae898649
Maxim <46995666+Vincenz099@users.noreply.github.com>
2019-03-13 11:34:09 +0100

    Added float compression and Quaternion compression
    Added float compression and Quaternion compression Floats use bounded range, Quaternions use set bits per component, default is 12 which results in a 38 bit usage per quaternion with very high accuracy, around 0.05 degrees error. possible is to supply less bits like for instance 10, which result in acuracy of avg error of 0.17 degrees.

--- BitBufferV2.cs
+++ BitBufferV2.cs
@@ -38,9 +38,14 @@
  *  3. This notice may not be removed or altered from any source distribution.
  */
 using System;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text;
+#if !(ENABLE_MONO || ENABLE_IL2CPP)
+using System.Diagnostics;
+using System.Numerics;
+#else
+using UnityEngine;
+#endif
 
 namespace NetStack.Serialization {
     public class BitBufferV2 {
@@ -697,7 +702,210 @@ namespace NetStack.Serialization {
 
             return value;
         }
-        
+
+        [MethodImpl(256)]
+        public BitBufferV2 AddFloat(float value, float min, float max, float precision)
+        {
+            float range = max - min;
+            float invPrecision = 1.0f / precision;
+            float maxVal = range * invPrecision;
+            int numBits = Log2((uint)(maxVal + 0.5f)) + 1;
+            float adjusted = (value - min) * invPrecision;
+
+            Add(numBits, (uint)(adjusted + 0.5f));
+
+            return this;
+        }
+
+        [MethodImpl(256)]
+        public BitBufferV2 AddFloat(float value, float min, float max, int numBits)
+        {
+            var maxvalue = (1 << numBits) - 1;
+
+            float range = max - min;
+            var precision = range / maxvalue;
+            var invPrecision = 1.0f / precision;
+
+            float adjusted = (value - min) * invPrecision;
+
+            Add(numBits, (uint)(adjusted + 0.5f));
+
+            return this;
+        }
+
+        [MethodImpl(256)]
+        public float ReadFloat(float min, float max, float precision)
+        {
+            float range = max - min;
+            float invPrecision = 1.0f / precision;
+            float maxVal = range * invPrecision;
+            int numBits = Log2((uint)(maxVal + 0.5f)) + 1;
+
+            return Read(numBits) * precision + min;
+        }
+
+        [MethodImpl(256)]
+        public float ReadFloat(float min, float max, int numBits) {
+            var maxvalue = (1 << numBits) - 1;
+            float range = max - min;
+            var precision = range / maxvalue;
+
+            return Read(numBits) * precision + min;
+        }
+
+        [MethodImpl(256)]
+        public float PeekFloat(float min, float max, float precision) {
+            float range = max - min;
+            float invPrecision = 1.0f / precision;
+            float maxVal = range * invPrecision;
+            int numBits = Log2((uint)(maxVal + 0.5f)) + 1;
+
+            return Peek(numBits) * precision + min;
+        }
+
+        [MethodImpl(256)]
+        public float PeekFloat(float min, float max, int numBits) {
+            var maxvalue = (1 << numBits) - 1;
+            float range = max - min;
+            var precision = range / maxvalue;
+
+            return Peek(numBits) * precision + min;
+        }
+
+        private const float SmallestThreeUnpack = 0.70710678118654752440084436210485f + 0.0000001f; // addition to rounding to fit in -0....1023 instead of 0...1024
+        private const float SmallestThreePack = 1f / SmallestThreeUnpack;
+
+        [MethodImpl(256)]
+        public BitBufferV2 AddQuaternion(Quaternion quaternion, int bitsPerComponent = 12) { 
+            float halfrangeFloat = (1 << bitsPerComponent - 1);
+            float packer = SmallestThreePack * halfrangeFloat;
+
+            uint m = 0, a, b, c;
+            float maxValue = float.MinValue;
+            bool signminus = false;
+
+            for (uint quadIndex = 0; quadIndex <= 3; quadIndex++) {
+                float element = 0f;
+                float abs = 0f;
+
+#if !(ENABLE_MONO || ENABLE_IL2CPP)
+                switch (quadIndex) {
+                    case 0:
+                        element = quaternion.X;
+                        break;
+                    case 1:
+                        element = quaternion.Y;
+                        break;
+                    case 2:
+                        element = quaternion.Z;
+                        break;
+                    case 3:
+                        element = quaternion.W;
+                        break;
+                }
+#else
+                switch (quadIndex) {
+                    case 0:
+                        element = quaternion.x;
+                        break;
+                    case 1:
+                        element = quaternion.y;
+                        break;
+                    case 2:
+                        element = quaternion.z;
+                        break;
+                    case 3:
+                        element = quaternion.w;
+                        break;
+                }
+#endif
+                abs = Math.Abs(element);
+
+                if (abs > maxValue) {
+                    signminus = (element < 0f);
+                    m = quadIndex;
+                    maxValue = abs;
+                }
+            }
+
+#if !(ENABLE_MONO || ENABLE_IL2CPP)
+            float aflt, bflt, cflt;
+            switch (m) {
+                case 0: aflt = quaternion.Y; bflt = quaternion.Z; cflt = quaternion.W; break;
+                case 1: aflt = quaternion.X; bflt = quaternion.Z; cflt = quaternion.W; break;
+                case 2: aflt = quaternion.X; bflt = quaternion.Y; cflt = quaternion.W; break;
+                default: aflt = quaternion.X; bflt = quaternion.Y; cflt = quaternion.Z; break;
+            }
+#else
+            float aflt, bflt, cflt;
+            switch (m) {
+                case 0: aflt = quaternion.y; bflt = quaternion.z; cflt = quaternion.w; break;
+                case 1: aflt = quaternion.x; bflt = quaternion.z; cflt = quaternion.w; break;
+                case 2: aflt = quaternion.x; bflt = quaternion.y; cflt = quaternion.w; break;
+                default: aflt = quaternion.x; bflt = quaternion.y; cflt = quaternion.z; break;
+            }
+#endif
+
+            if (signminus) {
+                a = (uint)((-aflt * packer) + halfrangeFloat);
+                b = (uint)((-bflt * packer) + halfrangeFloat);
+                c = (uint)((-cflt * packer) + halfrangeFloat);
+            }
+            else {
+                a = (uint)((aflt * packer) + halfrangeFloat);
+                b = (uint)((bflt * packer) + halfrangeFloat);
+                c = (uint)((cflt * packer) + halfrangeFloat);
+            }
+
+            Add(2, m);
+            Add(bitsPerComponent, a);
+            Add(bitsPerComponent, b);
+            Add(bitsPerComponent, c);
+
+            return this;
+        }
+
+        [MethodImpl(256)]
+        public Quaternion ReadQuaternion(int bitsPerValue) {
+            int halfrange = (1 << bitsPerValue - 1); //  - 1
+            float unpacker = SmallestThreeUnpack * (1f / halfrange);
+
+            uint m = Read(2);
+            int aint = (int)Read(bitsPerValue);
+            int bint = (int)Read(bitsPerValue);
+            int cint = (int)Read(bitsPerValue);
+
+            aint -= halfrange;
+            bint -= halfrange;
+            cint -= halfrange;
+
+            float a = aint * unpacker;
+            float b = bint * unpacker;
+            float c = cint * unpacker;
+
+            float d = (float)Math.Sqrt(1f - ((a * a) + (b * b) + (c * c)));
+
+            switch (m) {
+                case 0:
+                    return new Quaternion(d, a, b, c);
+                case 1:
+                    return new Quaternion(a, d, b, c);
+                case 2:
+                    return new Quaternion(a, b, d, c);
+                default:
+                    return new Quaternion(a, b, c, d);
+            }
+        }
+
+        [MethodImpl(256)]
+        public Quaternion PeekQuaternion(int bitsPerValue) {
+            var curReadpos = bitsRead;
+            var value = ReadQuaternion(bitsPerValue);
+
+            bitsRead = curReadpos;
+            return value;
+        }
+
         [MethodImpl(256)]
         public BitBufferV2 AddString(string value) {
             if (value == null)
